71809466368d42200bb517287239c6f90bf73621
Sach <tinatrenkner@gmail.com>
2016-10-13 14:33:47 +1300

    test rewritten, test passes

--- tests/index.js
+++ tests/index.js
@@ -1,27 +1,16 @@
 const test  = require('tape')
-const tapSpec = require('tap-spec')
-// You may need to set another const for relevant JS files
 
-test ('initial', function (t) {
-  t.plan(1)
-  t.equal('a', 'a')
+// import code to be tested
+
+test ('that tests are working', function (t) {
+  // arrange
+  t.plan(1) // don't need this if you use end()
+  var expected = 1
+
+  // act
+  var actual = 1
+
+  // assert
+  t.equal(actual, expected)
+  t.end() // don't need this if you use plan()
 })
-
-
-test('tell us what you are testing', function (t) {
-  var whatYoureTesting = [] // whatever value you want to test
-  var expected = expectedResult
-  var actual = nowTest(whatYoureTesting) //nowTest would be in your JS file
-  t.equal (actual, expected)
-  t.end()
-})
-
-// If you forgot, this is how a test can look:
-//
-// test('scores a gutterball frame', function (t) {
-//   var frame = [0, 0]
-//   var expected = 0
-//   var actual = game.scoreFrame(frame)
-//   t.equal(actual, expected)
-//   t.end()
-// })
