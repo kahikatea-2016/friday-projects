97b6a3e1c8ad540116e8e2ee659f3b4c89971b84
Sach <tinatrenkner@gmail.com>
2016-10-14 16:35:46 +1300

    list.jsx rendering

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import List from './List'
 
+const projects = [
+  {id: 1, title: 'Project One'},
+  {id: 2, title: 'Project Two'}
+]
+
 export default React.createClass({
   render () {
     return (
       <div>
         <h1>Friday Projects</h1>
-        <List />
+        <List projects={projects} />
+        <button>Add a project</button> | <button>Edit a project</button>
       </div>
     )
   }
--- client/components/List.jsx
+++ client/components/List.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
 export default React.createClass({
-
+  propTypes: {
+    projects: React.PropTypes.arrayOf(React.PropTypes.shape({
+      id: React.PropTypes.number,
+      title: React.PropTypes.string
+    }))
+  },
   render () {
     return (
       <div>
         <h2>Click below to check out some Friday Projects</h2>
         <ul>
-          <li>Project #1</li>
+          {this.props.projects.map(project => {
+            return (
+              <li key={project.id}><a href={`/details/${project.id}`}>{project.title}</a></li>
+            )
+          })}
         </ul>
       </div>
     )
