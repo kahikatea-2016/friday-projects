426acc177b057390a31df617ca26f8ede0aae7b6
tealiie <tealiie@live.com>
2016-10-14 15:21:06 +1300

    WIP create updateproject db test not working

--- server/db.js
+++ server/db.js
@@ -6,22 +6,24 @@ const db = knex(config)
 
 export default {
   getProjects: getProjects.bind(null, db),
-  getProjectsPromise: getProjectsPromise.bind(null, db)
+  updateProject: updateProject.bind(null, db)
 }
 
-export function getProjects (db, cb) {
-  return db()
-  .select('title', 'id')
-  .from('projects')
-  .then(projects => cb(null, projects))
-  .catch(err => cb(err))
-}
-
-export function getProjectsPromise (db) {
+export function getProjects (db) {
   return new Promise((resolve, reject) => {
     db().select('title', 'id')
       .from('projects')
       .then(resolve)
       .catch(reject)
+  })
+}
+
+export function updateProject (db) {
+  return new Promise((resolve, reject) => {
+    db().where('title', 'id', 'description', 'repo_url', 'app_url', 'date')
+    .from('projects')
+    .update('projects')
+    .then(resolve)
+    .catch(reject)
   })
 }
--- tests/db.js
+++ tests/db.js
@@ -1,7 +1,9 @@
 import test from 'tape'
 import knex from 'knex'
 
-import {getProjectsPromise} from '../server/db'
+// import {getProjects} from '../server/db'
+
+import {updateProject} from '../server/db'
 
 const testConfig = {
   client: 'sqlite3',
@@ -10,17 +12,36 @@ const testConfig = {
 }
 const testDb = knex(testConfig)
 
-test('getProjects', function (t) {
+// test('getProjects', function (t) {
+//   // arrange
+//   createTestData().then(() => {
+//     // act
+//     getProjects(testDb)
+//       .then(projects => {
+//         // assert
+//         t.equal(projects[0].id, 1)
+//         t.equal(projects[0].title, 'Project One')
+//         t.equal(projects[1].id, 2)
+//         t.equal(projects[1].title, 'Project Two')
+//         t.end()
+//       })
+//   })
+// })
+
+//
+test('updateProject', function (t) {
   // arrange
   createTestData().then(() => {
     // act
-    getProjectsPromise(testDb)
+    updateProject(testDb)
       .then(projects => {
         // assert
-        t.equal(projects[0].id, 1)
-        t.equal(projects[0].title, 'Project One')
-        t.equal(projects[1].id, 2)
-        t.equal(projects[1].title, 'Project Two')
+        t.equal(projects[0].title, '')
+        t.equal(projects[0].id, '')
+        t.equal(projects[0].description, '')
+        t.equal(projects[0].repo_url, '')
+        t.equal(projects[0].app_url, '')
+        t.equal(projects[0].date, '')
         t.end()
       })
   })
