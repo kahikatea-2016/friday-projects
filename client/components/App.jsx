5d2e81c9f22e0e374f1069a5227fe46565122b08
Sach <tinatrenkner@gmail.com>
2016-10-17 15:58:25 +1300

    removed redundant object

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,30 +2,6 @@ import React from 'react'
 import request from 'superagent'
 
 import List from './List'
-import Details from './Details'
-
-const projects = [
-  {
-    id: 1,
-    title: 'Project One',
-    description: 'This is project one',
-    date: '20/04/2016',
-    appUrl: 'www.facebook.com',
-    repoUrl: 'https://github.com/facebook',
-    photos: {
-      url: 'https://c2.staticflickr.com/8/7446/11127634193_1d074d28a3_k.jpg',
-      caption: 'This is a corgi, because I can.'
-    },
-    teamMembers: {
-      name: 'Bob'
-    }
-  },
-  {
-    id: 2,
-    title: 'Project Two',
-    description: 'This is project two'
-  }
-]
 
 export default React.createClass({
   getInitialState () {
