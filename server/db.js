f9544b78ae9326497f099c00af806631acf33a2c
Andrew Hird <andrew.james.hird@gmail.com>
2016-10-17 15:29:15 +1300

    WIP writing inserts

--- server/db.js
+++ server/db.js
@@ -1,3 +1,5 @@
+/* eslint camelcase: [2, {properties: "never"}] */
+
 const knex = require('knex')
 
 const config = require('../knexfile')[process.env.NODE_ENV || 'development']
@@ -6,7 +8,9 @@ const db = knex(config)
 
 module.exports = {
   _getProjects: _getProjects,
-  getProjects: _getProjects.bind(null, db)
+  getProjects: _getProjects.bind(null, db),
+  _addProject: _addProject,
+  addProject: _addProject.bind(null, db)
 }
 
 function _getProjects (db) {
@@ -16,4 +20,38 @@ function _getProjects (db) {
       .then(resolve)
       .catch(reject)
   })
+}
+
+function _addProject (project) {
+  return new Promise((resolve, reject) => {
+    knex('projects')
+    .insert({
+      title: project.title,
+      description: project.description,
+      repo_url: project.repoUrl,
+      app_url: project.appUrl,
+      date: project.date
+    })
+    .then(projectId => addTeamMembers(projectId, project.teamMembers))
+    .then(projectId => addPhotos(projectId, project.photoUrl, project.photoCaption))
+    .then(resolve)
+    .catch(reject)
+  })
+}
+
+function addTeamMembers (projectId, teamMembers) {
+  console.log('projectId: ', projectId)
+  return knex('team_members')
+    .insert({name: teamMembers})
+    .then(() => {
+      return projectId
+    })
+}
+
+function addPhotos (projectId, photosUrl, photosCaption) {
+  return knex('photos')
+    .insert({url: photosUrl, caption: photosCaption})
+    .then(() => {
+      return projectId
+    })
 }
